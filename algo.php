2180020e41a16418e099993112e2a4b725b2814c
moonye@live.fr <moonye@live.fr>
2024-12-04 11:40:34 +0100

    exo2

--- algo.php
+++ algo.php
@@ -6,17 +6,31 @@ echo "<br>";
 function exo1(): bool {
     return true; 
 }
+$varexo1 = exo1();  
 
-$varexo1 = exo1(); 
-
-// if ($varexo1 === true ){
-// echo "La fonction retourne true";
-// } else {
-//     echo "La fonction retourne faux";
-//     } 
+echo"{$varexo1} <br>";
+if ($varexo1) {
+    echo "La fonction retourne true";
+} else {
+    echo "La fonction retourne faux";
+}
 
 // EXERCICE 2
 echo "<br>";
 echo "<br>";
 echo "EXERCICE 2";
+echo "<br>";
+
+function exo2(): string {
+    return "C'est une chaine de charactère"; 
+}
+
+$varexo2 = exo2();
+
+echo $varexo2;
+
+// EXERCICE 3
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 3";
 echo "<br>";
