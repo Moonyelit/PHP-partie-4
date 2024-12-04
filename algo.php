03b99b5890265846412398b695a1dab3931ea4bd
moonye@live.fr <moonye@live.fr>
2024-12-04 11:33:49 +0100

    exo1

--- algo.php
+++ algo.php
@@ -1,2 +1,22 @@
 <?php
-// $exo1 = 0;
+// EXERCICE 1
+echo "EXERCICE 1";
+echo "<br>";
+
+function exo1(): bool {
+    return true; 
+}
+
+$varexo1 = exo1(); 
+
+// if ($varexo1 === true ){
+// echo "La fonction retourne true";
+// } else {
+//     echo "La fonction retourne faux";
+//     } 
+
+// EXERCICE 2
+echo "<br>";
+echo "<br>";
+echo "EXERCICE 2";
+echo "<br>";
